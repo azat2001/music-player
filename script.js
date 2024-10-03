c31af7863466f8a76e62a8b61c98108e64e48382
azat20014 <azat.garifullin.2001@yandex.ru>
2024-10-03 21:35:42 +0500

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fileUpload = document.getElementById('fileUpload');
+const uploadInput = document.getElementById('uploadInput');
 const uploadButton = document.getElementById('uploadButton');
 const syncButton = document.getElementById('syncButton');
 const audioPlayer = document.getElementById('audioPlayer');
@@ -6,36 +6,30 @@ const playlist = document.getElementById('playlist');
 
 // Функция для загрузки файлов на GitHub
 uploadButton.addEventListener('click', () => {
-  fileUpload.click();
+    uploadInput.click();
 });
 
-fileUpload.addEventListener('change', () => {
-  const files = fileUpload.files;
-  if (files.length > 0) {
-    // Здесь необходимо реализовать загрузку файлов на GitHub
-    // Например, используя GitHub API или библиотеки, такие как axios
-    // После загрузки обновите список воспроизведения
-  }
+uploadInput.addEventListener('change', () => {
+    const files = uploadInput.files;
+    // Здесь необходимо реализовать загрузку файлов на GitHub, например, с помощью GitHub API
 });
 
 // Функция для синхронизации с GitHub
 syncButton.addEventListener('click', () => {
-  // Здесь необходимо реализовать загрузку списка файлов с GitHub
-  // и обновление списка воспроизведения
-  // Например, используя GitHub API для получения списка файлов в репозитории
-  // и добавления их в список воспроизведения
+    // Получаем список файлов с GitHub и обновляем плейлист
+    // Пример: использование Fetch API для получения данных с GitHub
+    fetch('https://api.github.com/repos/azat2001/music-player/contents/music_folder')
+        .then(response => response.json())
+        .then(data => {
+            playlist.innerHTML = '';
+            data.forEach(file => {
+                const listItem = document.createElement('li');
+                listItem.textContent = file.name;
+                listItem.addEventListener('click', () => {
+                    audioPlayer.src = `https://raw.githubusercontent.com/azat2001/music-player/main/music_folder/${file.name}`;
+                    audioPlayer.play();
+                });
+                playlist.appendChild(listItem);
+            });
+        });
 });
-
-// Функция для обновления списка воспроизведения
-function updatePlaylist(files) {
-  playlist.innerHTML = '';
-  files.forEach(file => {
-    const listItem = document.createElement('li');
-    listItem.textContent = file.name;
-    listItem.addEventListener('click', () => {
-      audioPlayer.src = `путь_к_файлу/${file.name}`;
-      audioPlayer.play();
-    });
-    playlist.appendChild(listItem);
-  });
-}
